6cc52535657e7d12f658d4e74f97e283fee085a3
Thanh <hhthanh.hust@gmail.com>
2019-10-13 01:01:27 +0900

    fix

--- adapter.py
+++ adapter.py
@@ -3,19 +3,8 @@ import sys
 import logging
 from logging.handlers import RotatingFileHandler
 
-logger = logging.getLogger(__name__)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-logger.setLevel(logging.DEBUG)
-
-log_handler = RotatingFileHandler('linebot.log', maxBytes=1024, backupCount=5)
-log_handler.setFormatter(formatter)
 sys.path.insert(0, '/var/www/line-bot-flask')
-logger.addHandler(log_handler)
-try:
-	from application import application
-	if __name__ == '__main__':
-		application.run()
-except Exception as e:
-	logger.exception(e)
-	logger.info(sys.version)
-	logger.info(sys.path)
+from application import application
+
+if __name__ == '__main__':
+	application.run()

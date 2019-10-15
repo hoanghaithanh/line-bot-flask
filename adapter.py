607995fb639e85d75d545400832e5225292cb1b4
hrs-hthanh <hthanh@hr-s.co.jp>
2019-10-15 18:21:59 +0900

    fix

--- adapter.py
+++ adapter.py
@@ -5,5 +5,7 @@ from application import application
 
 if __name__ == '__main__':
 	db.init_app(application)
-	db.create_all()
+	#Tell sqlalchemy which app is the current app
+	with application.app_context():
+		db.create_all()
 	application.run()

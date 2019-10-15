6c7855ce3f80e14047172cf1de4223a2fe42e2cf
hrs-hthanh <hthanh@hr-s.co.jp>
2019-10-15 18:09:03 +0900

    fix

--- adapter.py
+++ adapter.py
@@ -3,7 +3,6 @@
 from application import application
 from app.extentions import db
 
-if __name__ == '__main__':
-	db.init_app(application)
-	db.create_all()
-	application.run()
+db.init_app(application)
+db.create_all()
+application.run()

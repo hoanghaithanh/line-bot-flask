aa6ba90345ddb79de06b2a983cfcbc33bade82fc
hrs-hthanh <hthanh@hr-s.co.jp>
2019-10-15 18:16:51 +0900

    fix

--- adapter.py
+++ adapter.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
-from application import application
 from app.extentions import db
+from application import application
 
-db.init_app(application)
-db.create_all()
+if __name__ == '__main__':
+	db.init_app(application)
+	db.create_all()
+	application.run()
